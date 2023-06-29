517232c16f579cdff2337a9a9d2f987286cb2011
DOUCE_LLC <matiasaguilarprogramacion@gmail.com>
2023-06-29 12:37:41 -0300

    m2

---
+++ 2 Second Module/script.sql
@@ -0,0 +1,40 @@
+-- Responder Verdadero o Falso
+-- 1. Todas las métricas no son KPIs, pero todos los KPIs están basados en métricas.
+
+-- Elegir la opción correcta
+-- 2. El espacio muestral es:
+
+-- Opciones:
+-- 1- El conjunto de todos los resultados posibles de un experimento.
+-- 2- Una tabla con las distintas funciones de probabilidad.
+-- 3- Un evento que puede describirse con una característica única.
+
+-- 3. Si una empresa indica "nuestra retención de clientes ha aumentado un 20% respecto al año anterior, tal como nos habíamos propuesto", ¿De qué está hablando?
+
+-- Opciones:
+-- a - Métrica
+-- b- KPI
+
+-- Responde:
+-- 4. Acabo de llegar al casino, y en la mesa de ruleta clásica (37 números, 0-36) acaba de salir el 3. ¿Cuál es la probabilidad de que se repita este número, saliendo en el próximo tiro?
+
+
+
+-- Utilizando MySQL responde las siguientes preguntas:
+
+-- 5. ¿Cuál es el canal de venta que tiene la segunda posición en cantidad de ventas en 2020?
+-- Opciones:
+-- 1- Telefónica
+-- 2- OnLine
+-- 3- Presencial
+
+-- 6. ¿Cuantos productos tienen la palabra CD en alguna parte de su descripción (Concepto = Descripción del Producto) y su precio es mayor a 500?
+
+-- 7. Considerando el año 2017, ¿Cuál fue el mes (considerando la fecha de venta, es decir, usando el campo Fecha) con mayor monto total de venta (monto de venta = Precio*Cantidad) para el empleado 1426?
+
+-- Opciones:
+-- a - 5
+-- b - 4
+-- c - 7
+-- d - 3
+-- e - 11
